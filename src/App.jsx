2bb46b2ecb8eb4a64c40a2b335972a0d74ea442a
Momen <touristmomen@gmail.com>
2025-02-22 12:57:42 +0600

    redux count setup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./features/counter/counterSlice";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="text-center h-screen flex justify-center flex-col">
       <div className="flex items-center justify-center gap-5">
         <button
           className="bg-green-500 px-2 py-1 rounded-md"
-          onClick={() => setCount((count) => count + 1)}
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
         >
           Increase
         </button>
         <p>Count is {count}</p>
         <button
           className="bg-red-500 px-2 py-1 rounded-md"
-          onClick={() => setCount((count) => count - 1)}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
         >
           Decrease
         </button>
